e4e668f087d87af4ab18360fb47ced3061383700
KovD <kovacsd435@gmail.com>
2026-03-31 08:12:40 +0200

    C# my querry

--- F9Y7TW_0328/My_pogram.cs
+++ F9Y7TW_0328/My_pogram.cs
@@ -1,7 +1,103 @@
+using System;
 using System.Xml.Linq;
 using System.Linq;
 
 XDocument dokumentum = XDocument.Load("F9Y7TW_XML1.xml");
 XElement gyoker = dokumentum.Descendants("vendeglatas").First();
 
-Console.WriteLine(gyoker);
+Console.WriteLine("(0.) Dokumentum betöltve.");
+
+
+Console.WriteLine("\n(1.) Budapesti luxusbárok:");
+var bpBarok = gyoker.Descendants("luxusbar")
+    .Where(elem => elem.Element("varos").Value == "Budapest").ToList();
+
+foreach (var item in bpBarok)
+{
+    Console.WriteLine("-- " + item.Element("nev").Value);
+}
+
+Console.WriteLine("\n(2.) Termékek és beszállítóik (JOIN):");
+var termekJoin = 
+    from t in gyoker.Descendants("termek")
+    join sz in gyoker.Descendants("besszallitoceg") on (string)t.Attribute("SzallID") equals (string)sz.Attribute("SzallID")
+    select new
+    {
+        TermekNeve = (string)t.Element("termeknev"),
+        Ar = (string)t.Element("Ar_db"),
+        Beszallito = (string)sz.Element("nev")
+    };
+
+foreach (var item in termekJoin)
+{
+    Console.WriteLine($"{item.TermekNeve} ({item.Ar} Ft) - Beszállító: {item.Beszallito}");
+}
+
+Console.WriteLine("\n(3.) Aggregáció:");
+var atlag_ar = gyoker.Descendants("termek")
+    .Select(t => t.Element("Ar_db").Value)
+    .Average(ar => double.Parse(ar));
+
+Console.WriteLine($"(3.1) A termékek átlagos ára: {atlag_ar} Ft");
+
+Console.WriteLine("\n(4.) Minden termék árát megduplázom, majd elmentem egy új fájlba...");
+
+gyoker.Descendants("termek")
+    .ToList()
+    .ForEach(t => 
+    {
+        var arElem = t.Element("Ar_db");
+        var ar = double.Parse(arElem.Value);
+        ar *= 2;
+        arElem.Value = ar.ToString();
+    });
+
+XDocument very_italokDokumentum = new XDocument(gyoker);
+very_italokDokumentum.Save("draga_italok.xml");
+
+Console.WriteLine("Mentés sikeres: draga_italok.xml");
+Console.WriteLine("\n(5.) 40 évnél idősebb patrónusok:");
+var idosPatronok = gyoker.Descendants("patron")
+    .Where(p => int.Parse(p.Element("kor").Value) > 40)
+    .Select(p => $"{p.Element("nev").Element("vezeteknev").Value} {p.Element("nev").Element("keresztnev").Value}");
+
+foreach (var p in idosPatronok) Console.WriteLine($"-- {p}");
+
+
+Console.WriteLine("\n(6.) Termékek allergének szerint:");
+var allergenCsoportok = gyoker.Descendants("termek")
+    .GroupBy(t => t.Element("allergen").Value);
+
+foreach (var csoport in allergenCsoportok)
+{
+    Console.WriteLine($"[{csoport.Key}]");
+    foreach (var t in csoport) Console.WriteLine($" - {t.Element("termeknev").Value}");
+}
+
+Console.WriteLine("\n(7.) Budapesti dolgozók:");
+var bpDolgozok = gyoker.Descendants("dolgozo")
+    .Where(d => d.Element("lakcim").Value.Contains("Budapest"))
+    .Select(d => $"{d.Element("nev").Element("vezeteknev").Value} {d.Element("nev").Element("keresztnev").Value}");
+
+foreach (var d in bpDolgozok) Console.WriteLine($"-- {d}");
+
+Console.WriteLine("\n(8.) Patrónusok és a bárok, ahova be vannak jelentve:");
+var bejelentkezesek = 
+    from b in gyoker.Descendants("bejelentve")
+    join p in gyoker.Descendants("patron") on (string)b.Attribute("b_p") equals (string)p.Attribute("PatID")
+    join l in gyoker.Descendants("luxusbar") on (string)b.Attribute("b_k") equals (string)l.Attribute("BarID")
+    select new { Vendeg = (string)p.Element("nev").Element("vezeteknev"), Bar = (string)l.Element("nev") };
+
+foreach (var item in bejelentkezesek) Console.WriteLine($"-- {item.Vendeg} -> {item.Bar}");
+
+Console.WriteLine("\n(9.) Van-e 100 db-nál kevesebb készletű termék?");
+bool vanKeves = gyoker.Descendants("termek").Any(t => int.Parse(t.Element("mennyiseg").Value) < 100);
+Console.WriteLine($"-- {(vanKeves ? "Igen" : "Nem")}");
+
+
+Console.WriteLine("\n(10.) Termékek árcsökkenő sorrendben:");
+var rendezettTermekek = gyoker.Descendants("termek")
+    .OrderByDescending(t => int.Parse(t.Element("Ar_db").Value))
+    .Select(t => $"{t.Element("termeknev").Value} - {t.Element("Ar_db").Value} Ft");
+
+foreach (var t in rendezettTermekek) Console.WriteLine($"-- {t}");
--- F9Y7TW_0328/Program.cs
+++ F9Y7TW_0328/Program.cs
@@ -1,92 +1,92 @@
-﻿using System.Xml.Linq;
-using System.Linq;
+﻿// using System.Xml.Linq;
+// using System.Linq;
 
-XDocument dokumentum = XDocument.Load("F9Y7TW_XML.xml");
-XElement gyoker = dokumentum.Descendants("vendeglatas").First();
+// XDocument dokumentum = XDocument.Load("F9Y7TW_XML.xml");
+// XElement gyoker = dokumentum.Descendants("vendeglatas").First();
 
-Console.WriteLine("(0.) A doc \n" + gyoker);
+// Console.WriteLine("(0.) A doc \n" + gyoker);
 
-Console.WriteLine("(1.) Le 5 csillagos édtermek");
+// Console.WriteLine("(1.) Le 5 csillagos édtermek");
 
-var five_star = gyoker.Descendants("etterem").Where(
-    elem => elem.Descendants("csillag").First().Value == "5").ToList();
+// var five_star = gyoker.Descendants("etterem").Where(
+//     elem => elem.Descendants("csillag").First().Value == "5").ToList();
 
-foreach (var item in five_star)
-{
-    Console.WriteLine("--" + item.Descendants("nev").First().Value);
-}
+// foreach (var item in five_star)
+// {
+//     Console.WriteLine("--" + item.Descendants("nev").First().Value);
+// }
 
-Console.WriteLine("(3.) Ki, mit mennyiért?");
+// Console.WriteLine("(3.) Ki, mit mennyiért?");
 
-var three_join = gyoker.Descendants("rendeles").Select(elem =>
-{
-    var vendegID = elem.Attribute("vkod").Value;
-    var vendeg = gyoker.Descendants("vendeg")
-        .Where(vendegElem => vendegElem.Attribute("vkod").Value == vendegID)
-        .First().Descendants("nev").FirstOrDefault().Value;
+// var three_join = gyoker.Descendants("rendeles").Select(elem =>
+// {
+//     var vendegID = elem.Attribute("vkod").Value;
+//     var vendeg = gyoker.Descendants("vendeg")
+//         .Where(vendegElem => vendegElem.Attribute("vkod").Value == vendegID)
+//         .First().Descendants("nev").FirstOrDefault().Value;
 
-    var etteremID = elem.Attribute("ekod").Value;
-    var etterem = gyoker.Descendants("etterem")
-        .First(etteremElem => etteremElem.Attribute("ekod").Value == etteremID)
-        .Descendants("nev").FirstOrDefault().Value;
+//     var etteremID = elem.Attribute("ekod").Value;
+//     var etterem = gyoker.Descendants("etterem")
+//         .First(etteremElem => etteremElem.Attribute("ekod").Value == etteremID)
+//         .Descendants("nev").FirstOrDefault().Value;
 
-    var rendeltEtel = elem.Descendants("etel").First().Value;
-    var osszeg = elem.Descendants("osszeg").First().Value;
+//     var rendeltEtel = elem.Descendants("etel").First().Value;
+//     var osszeg = elem.Descendants("osszeg").First().Value;
 
-    return new
-    {
-        Vendeg = vendeg,
-        Etterem = etterem,
-        Etel = rendeltEtel,
-        Osszeg = osszeg
-    };
-}
-).ToList();
+//     return new
+//     {
+//         Vendeg = vendeg,
+//         Etterem = etterem,
+//         Etel = rendeltEtel,
+//         Osszeg = osszeg
+//     };
+// }
+// ).ToList();
 
-three_join.ForEach(join => Console.WriteLine(join));
+// three_join.ForEach(join => Console.WriteLine(join));
 
-//or easier if you have new .NET
+// //or easier if you have new .NET
 
-Console.WriteLine("-----------------------Easier-----------------------------");
+// Console.WriteLine("-----------------------Easier-----------------------------");
 
-var harmasJoin = 
-    from r in gyoker.Descendants("rendeles")
-    join v in gyoker.Descendants("vendeg") on (string)r.Attribute("vkod") equals (string)v.Attribute("vkod")
-    join e in gyoker.Descendants("etterem") on (string)r.Attribute("ekod") equals (string)e.Attribute("ekod")
-    select new
-    {
-        VendegNeve = (string)v.Element("nev"),
-        EtteremNeve = (string)e.Element("nev"),
-        Etel = (string)r.Element("etel"),
-        Ar = (string)r.Element("osszeg")
-    };
+// var harmasJoin = 
+//     from r in gyoker.Descendants("rendeles")
+//     join v in gyoker.Descendants("vendeg") on (string)r.Attribute("vkod") equals (string)v.Attribute("vkod")
+//     join e in gyoker.Descendants("etterem") on (string)r.Attribute("ekod") equals (string)e.Attribute("ekod")
+//     select new
+//     {
+//         VendegNeve = (string)v.Element("nev"),
+//         EtteremNeve = (string)e.Element("nev"),
+//         Etel = (string)r.Element("etel"),
+//         Ar = (string)r.Element("osszeg")
+//     };
 
-foreach (var item in harmasJoin)
-{
-    Console.WriteLine($"{item.VendegNeve} rendelt: {item.Etel} ({item.Ar} Ft) - Helyszín: {item.EtteremNeve}");
-}
+// foreach (var item in harmasJoin)
+// {
+//     Console.WriteLine($"{item.VendegNeve} rendelt: {item.Etel} ({item.Ar} Ft) - Helyszín: {item.EtteremNeve}");
+// }
 
-Console.WriteLine("(4.) Aggregáció\n");
+// Console.WriteLine("(4.) Aggregáció\n");
 
-var atlagKoltes = gyoker.Descendants("rendeles")
-    .Select(rendeles => rendeles.Descendants("osszeg").First().Value)
-    .Average(osszeg => double.Parse(osszeg));
+// var atlagKoltes = gyoker.Descendants("rendeles")
+//     .Select(rendeles => rendeles.Descendants("osszeg").First().Value)
+//     .Average(osszeg => double.Parse(osszeg));
 
-Console.WriteLine($"(4.1) Az átlagos költés: {atlagKoltes}");
-Console.WriteLine("(4.2) Minden rendelés összegét megduplázom, majd elmentem egy új fájlba: \n");
+// Console.WriteLine($"(4.1) Az átlagos költés: {atlagKoltes}");
+// Console.WriteLine("(4.2) Minden rendelés összegét megduplázom, majd elmentem egy új fájlba: \n");
 
-gyoker.Descendants("rendeles")
-    .ToList()
-    .ForEach (rendeles => {
-        var osszegElem = rendeles.Descendants("osszeg").First();
-        var osszeg = double.Parse(osszegElem.Value);
-        osszeg *= 2;
-        osszegElem.Value = osszeg.ToString();
+// gyoker.Descendants("rendeles")
+//     .ToList()
+//     .ForEach (rendeles => {
+//         var osszegElem = rendeles.Descendants("osszeg").First();
+//         var osszeg = double.Parse(osszegElem.Value);
+//         osszeg *= 2;
+//         osszegElem.Value = osszeg.ToString();
 
-    }
-);
+//     }
+// );
 
-Console.WriteLine(gyoker);
+// Console.WriteLine(gyoker);
 
-XDocument modositottDokumentum = new XDocument(gyoker);
-modositottDokumentum.Save("very_drága_libamáj.xml");
+// XDocument modositottDokumentum = new XDocument(gyoker);
+// modositottDokumentum.Save("very_drága_libamáj.xml");
